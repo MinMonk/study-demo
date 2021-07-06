9a0ec8425b56cd62100149db7e7a73fab21dc112
monk <459506421@qq.com>
2021-07-06 23:19:44 +0800

    PriorityBlockingQueue测试代码提交

--- spring-boot-demo/src/test/java/com/monk/utils/queue/PriorityBlockingQueueTest.java
+++ spring-boot-demo/src/test/java/com/monk/utils/queue/PriorityBlockingQueueTest.java
@@ -4,134 +4,78 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Iterator;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
-class Producer implements Runnable{
-    
-    private ArrayBlockingQueue<String> queue;
-    
-    Producer(ArrayBlockingQueue<String> queue){
-        this.queue = queue;
-    }
-    
-    private AtomicInteger num = new AtomicInteger(0);
+public class PriorityBlockingQueueTest {
 
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                String msg = "msg_" + num.getAndIncrement();
-                queue.put(msg);
-                log.info("{}往队列中写入了消息{},队列大小:{}", Thread.currentThread().getName(), msg, queue.size());
-                // 当总共发送了30条消息的时候，就退出
-                if(num.get() > 30) {
-                    System.exit(0);
-                }
-            }
-        } catch (InterruptedException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-}
-
-@Slf4j
-class Consumer implements Runnable{
-    
-    private ArrayBlockingQueue<String> queue;
-    
-    Consumer(ArrayBlockingQueue<String> queue){
-        this.queue = queue;
-    }
-
-    @Override
-    public void run() {
-        try {
-            while(true) {
-                String msg = queue.take();
-                log.info("{}消费了消息：{}", Thread.currentThread().getName(), msg);
-            }
-        } catch (InterruptedException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-    
-}
-
-
-@Slf4j
-public class ArrayBlockingQueueTest {
-
-    private final static ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<String>(10);
+    private PriorityBlockingQueue<String> queue = new PriorityBlockingQueue<String>(10);
 
     /**
-     * 测试生产者-消费者模式
-     * @author Monk
-     * @date 2021年7月5日 下午4:02:06
+     * 测试add方法超过了队列初始化的长度,是否会抛异常
+     * 经过测试,不会抛出异常,会自动扩容
      */
     @Test
-    public void testProducerAndConsumer() throws Exception {
-        new Thread(new Producer(queue), "producer_1").start();
-        new Thread(new Consumer(queue), "consumer_1").start();
-        new Thread(new Consumer(queue), "consumer_2").start();
-        while(true);
+    public void testAddMethod() {
+        for (int i = 0; i < 11; i++) {
+            queue.add("msg_" + i);
+            log.info("add msg_{}", i);
+        }
+        assertEquals(11,queue.size());
+
+        // poll方法从队列中取消息是有序的
+        while (queue.size() > 0){
+            String msg = queue.poll();
+            if(null == msg) continue;
+            log.info("poll msg:{}", msg);
+        }
+
+        // Iterator从队列中取消息是没有顺序的
+        /*Iterator<String> iterator = queue.iterator();
+        while(iterator.hasNext()) {
+            log.info("iterator read msg:{}", iterator.next());
+        }*/
+
     }
 
-    @Test
-    public void testAddMethod() {
-        assertThrows(IllegalStateException.class, () -> {
-            for (int i = 0; i < 11; i++) {
-                queue.add("msg" + i);
-                log.info("msg_{}", i);
-            }
-        });
-    }
-    
     /**
      * 当队列满了，会一直阻塞
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:37:59
      */
     @Test
     public void testPutMethod() {
-        assertThrows(IllegalStateException.class, () -> {
-            for (int i = 0; i < 11; i++) {
-                queue.put("msg" + i);
-                log.info("msg_{}", i);
-            }
-        });
+        for (int i = 0; i < 11; i++) {
+            queue.put("msg" + i);
+            log.info("msg_{}", i);
+        }
+        assertEquals(11,queue.size());
     }
-    
+
     /**
      * 阻塞2s后返回失败
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:43:48
      */
     @Test
     public void testOfferMethod() {
         boolean flag = true;
-        try {
-            for (int i = 0; i < 11; i++) {
-                flag = queue.offer("msg" + i, 2, TimeUnit.SECONDS);
-                log.info("msg_{}, flag:{}", i, flag);
-            }
-        } catch (InterruptedException e) {
-            log.error(e.getMessage(), e);
+        for (int i = 0; i < 11; i++) {
+            flag = queue.offer("msg" + i, 2, TimeUnit.SECONDS);
+            log.info("msg_{}, flag:{}", i, flag);
         }
-        
-        assertEquals(false, flag);
+
+        assertEquals(11, queue.size());
     }
-    
+
     /**
      * 测试take方法，取去队首的元素并返回出来(如果队列为空，则阻塞线程)
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:55:09
      */
@@ -141,7 +85,7 @@ public class ArrayBlockingQueueTest {
         queue.add("msg_1");
         queue.add("msg_2");
         queue.add("msg_3");
-        
+
         try {
             String msg = queue.take();
             log.info("take msg:{}, queue size:{}", msg, queue.size());
@@ -151,11 +95,11 @@ public class ArrayBlockingQueueTest {
             log.error(e.getMessage(), e);
         }
     }
-    
-    
+
+
     /**
      * 测试poll方法，取去队首的元素并返回出来(如果队列为空，返回null)
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:54:02
      */
@@ -165,16 +109,16 @@ public class ArrayBlockingQueueTest {
         queue.add("msg_1");
         queue.add("msg_2");
         queue.add("msg_3");
-        
+
         String msg = queue.poll();
         log.info("take msg:{}, queue size:{}", msg, queue.size());
         assertEquals("msg_1", msg);
         assertEquals(2, queue.size());
     }
-    
+
     /**
      * 测试删除方法，删除队首的元素并返回出来
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:53:15
      */
@@ -184,16 +128,16 @@ public class ArrayBlockingQueueTest {
         queue.add("msg_1");
         queue.add("msg_2");
         queue.add("msg_3");
-        
+
         String msg = queue.remove();
         log.info("take msg:{}, queue size:{}", msg, queue.size());
         assertEquals("msg_1", msg);
         assertEquals(2, queue.size());
     }
-    
+
     /**
      * 测试删除一个存在的元素
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:53:15
      */
@@ -203,16 +147,16 @@ public class ArrayBlockingQueueTest {
         queue.add("msg_1");
         queue.add("msg_2");
         queue.add("msg_3");
-        
+
         boolean flag = queue.remove("msg_2");
         log.info("remove flag:{}, queue size:{}", flag, queue.size());
         assertEquals(true, flag);
         assertEquals(2, queue.size());
     }
-    
+
     /**
      * 测试删除一个不存在的元素
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:53:15
      */
@@ -222,16 +166,16 @@ public class ArrayBlockingQueueTest {
         queue.add("msg_1");
         queue.add("msg_2");
         queue.add("msg_3");
-        
+
         boolean flag = queue.remove("msg_4");
         log.info("remove flag:{}, queue size:{}", flag, queue.size());
         assertEquals(false, flag);
         assertEquals(3, queue.size());
     }
-    
+
     /**
      * 测试peek方法，查询队首的元素，只是查看，并不删除队首的元素
-     * 
+     *
      * @author Monk
      * @date 2021年7月5日 下午4:56:26
      */
@@ -241,13 +185,13 @@ public class ArrayBlockingQueueTest {
         queue.add("msg_1");
         queue.add("msg_2");
         queue.add("msg_3");
-        
+
         String msg = queue.peek();
         log.info("peek msg:{}, queue size:{}", msg, queue.size());
         assertEquals("msg_1", msg);
         assertEquals(3, queue.size());
     }
-    
+
     @Test
     public void testIteratorMethod() {
         //1. 添加测试数据
@@ -260,4 +204,5 @@ public class ArrayBlockingQueueTest {
             log.info("iterator msg:{}", iterator.next());
         }
     }
+
 }
