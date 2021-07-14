5b48f7e93ce7ed6a14169b71a05a5100d7ce7c40
monk <459506421@qq.com>
2021-07-14 23:10:40 +0800

    reentrantLock加锁测试代码提交,可debug调试细看

--- spring-boot-demo/src/test/java/com/monk/test/lock/ReentrantLockTest.java
+++ spring-boot-demo/src/test/java/com/monk/test/lock/ReentrantLockTest.java
@@ -1,6 +1,9 @@
 package com.monk.test.lock;
 
-import java.util.concurrent.locks.Condition;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -11,9 +14,40 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReentrantLockTest {
     
-    private final ReentrantLock lock = new ReentrantLock();
-    
-    private Condition condition = lock.newCondition();
-    
+    private static Lock lock = new ReentrantLock();
+    private static int sum = 0;
+
+    @Test
+    public void test(){
+        /**
+         * 使用monitorenter 和 monitorexit来实现加锁
+         */
+        synchronized (this){
+            sum++;
+        }
+    }
+
+    public static void main(String[] args){
+        for (int i = 0; i < 3; i++) {
+            new Thread(()->{
+                try {
+                    lock.lock();
+                    for (int j = 0; j < 100; j++) {
+                        sum++;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }).start();
+        }
+
+        try {
+            // 休眠1s,让线程执行完成
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sum);
+    }
 
 }
